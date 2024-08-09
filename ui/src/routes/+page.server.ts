a3653b8b21eb385499f8ed2a6ecd5dc47cc6dc63
Cole (Mike) Winberry <86802655+mike-winberry@users.noreply.github.com>
2024-08-08 21:28:47 -0700

    feat: #68 init golang static server and create initial zarf-package (#98)
    Co-authored-by: Andrew Greene <andrew@defenseunicorns.com>

--- ui/src/routes/+page.server.ts
+++ ui/src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-	throw redirect(307, '/apps');
+export const load: PageServerLoad = async ({ url }) => {
+	if (url.pathname === '/') {
+		throw redirect(307, '/apps');
+	}
 };
