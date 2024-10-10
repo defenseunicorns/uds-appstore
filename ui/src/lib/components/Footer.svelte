cc58481b18d6ea7aab8dfbcff1461e97532314a0
Andrew Risse <andrewrisse@gmail.com>
2024-10-10 09:06:17 -0600

    style wip

--- ui/src/lib/components/Footer.svelte
+++ ui/src/lib/components/Footer.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
   import { base } from '$app/paths';
+
+  // TODO - continue work on responsiveness
+  // TODO - test
 </script>
 
 <div
-  class=" flex w-full flex-col items-center justify-center gap-5 bg-[url('/images/doug_world_official_faded.png')] bg-auto"
+  class=" flex w-full flex-col items-center justify-center gap-5 bg-[url('/images/doug_world_official_faded.png')] bg-no-repeat"
 >
-  <div class="flex w-4/5 items-center justify-between">
+  <div class="flex w-11/12 flex-col items-center justify-between sm:flex-row lg:w-4/5">
     <img
       alt="unicorn guarantee"
       src="{base}/images/unicorn_guarantee.png"
       class="h-auto w-[25%] py-4 xl:py-8"
     />
-    <div class="flex flex-col items-center gap-2 pt-4 xl:pt-8">
+    <div class="flex flex-col items-center gap-2 pt-4 pb-[0.87rem] xl:pt-8">
       <img alt="within 2 weeks" src="{base}/images/within_2_weeks.png" class="h-auto w-[33%]" />
       <div class="text-center text-sm font-medium leading-[150%] xl:text-xl">
         <div>Your chosen applications will be deliverable to your target environment</div>
