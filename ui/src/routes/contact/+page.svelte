6e190bad88eb255d4c716d51eccf7d500a3b31c1
Andrew Risse <andrewrisse@gmail.com>
2024-10-10 21:16:42 -0600

    lint attempt 2, local lint seems fine, workflow failing though

--- ui/src/routes/contact/+page.svelte
+++ ui/src/routes/contact/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
   import { Alert, Button } from '$lib/components';
+
+  const message =
+    "We're currently developing this section to provide you with a direct line of communication with our team. In the meantime, our mission experts are ready to assist you with any specific authorization documentation or information you need for this application deployed on UDS.";
 </script>
 
 <div class="flex w-full justify-center">
-  <Alert
-    color="info"
-    title="Information Coming Soon!"
-    class="mt-4 w-10/12"
-    message="We're currently developing this section to provide you with a direct line of communication
-      with out team. In the meantime, our mission experts are ready to assist you with any
-      specific authorization documentation or information you need for this application
-      deployed on UDS."
-  >
+  <Alert color="info" title="Information Coming Soon!" class="mt-4 w-10/12" {message}>
     <div class="flex gap-2">
       <Button size="xs" color="alternative" href="https://www.defenseunicorns.com/contactus">
         Request More Information
