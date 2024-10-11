e14df15ae389fa29f5c8a67be73543fdef2eb099
Andrew Risse <andrewrisse@gmail.com>
2024-10-10 20:14:49 -0600

    lint

--- ui/src/routes/benefits/+page.svelte
+++ ui/src/routes/benefits/+page.svelte
@@ -7,7 +7,7 @@
     color="info"
     title="Information Coming Soon!"
     message="We're currently developing this section to provide you with detailed UDS information. 
-        In the meantime, our mission experts are ready to assist you with any
+    In the meantime, our mission experts are ready to assist you with any
     specific authorization documentation or information you need for this application
     deployed on UDS."
     class="mt-4 w-10/12"
