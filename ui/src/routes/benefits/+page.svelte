3e89090e7e9e191e1d0b180f7334871c94519dd8
Andrew Risse <andrewrisse@gmail.com>
2024-10-10 21:20:44 -0600

    merge alert changes 2

--- ui/src/routes/benefits/+page.svelte
+++ ui/src/routes/benefits/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
   import { Alert, Button } from '$lib/components';
+
+  const message =
+    "We're currently developing this section to provide you with detailed UDS information. In the meantime, our mission experts are ready to assist you with any specific authorization documentation or information you need for this application deployed on UDS.";
 </script>
 
 <div class="flex w-full justify-center">
-  <Alert
-    color="info"
-    title="Information Coming Soon!"
-    message="We're currently developing this section to provide you with detailed UDS information. 
-    In the meantime, our mission experts are ready to assist you with any
-    specific authorization documentation or information you need for this application
-    deployed on UDS."
-    class="mt-4 w-10/12"
-  >
+  <Alert color="info" title="Information Coming Soon!" class="mt-4 w-10/12" {message}>
     <div class="flex gap-2">
       <Button size="xs" color="alternative" href="https://www.defenseunicorns.com/contactus">
         Request More Information
