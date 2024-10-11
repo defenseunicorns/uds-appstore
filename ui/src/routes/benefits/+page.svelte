61f8e7312d2c187c12f93899662374f9e2675a3c
Andrew Risse <andrewrisse@gmail.com>
2024-10-10 21:13:19 -0600

    lint attempt, local lint seems fine, workflow failing though

--- ui/src/routes/benefits/+page.svelte
+++ ui/src/routes/benefits/+page.svelte
@@ -6,11 +6,11 @@
   <Alert
     color="info"
     title="Information Coming Soon!"
-    message="We're currently developing this section to provide you with detailed UDS information. 
-    In the meantime, our mission experts are ready to assist you with any
-    specific authorization documentation or information you need for this application
-    deployed on UDS."
     class="mt-4 w-10/12"
+    message="We're currently developing this section to provide you with detailed UDS information.
+      In the meantime, our mission experts are ready to assist you with any
+      specific authorization documentation or information you need for this application
+      deployed on UDS."
   >
     <div class="flex gap-2">
       <Button size="xs" color="alternative" href="https://www.defenseunicorns.com/contactus">
