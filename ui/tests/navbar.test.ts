ef0027910f514432cacc41e940c20f14aadc6fa9
Andrew Risse <52644157+andrewrisse@users.noreply.github.com>
2024-10-11 10:50:48 -0600

    feat(ui): navbar buttons (#304)

--- ui/tests/navbar.test.ts
+++ ui/tests/navbar.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from '@playwright/test';
 
-test('Check all footer links are working', async ({ page }) => {
+test('Check navbar links and button are working', async ({ page }) => {
   await page.goto('/apps');
 
-  const footer = await page.waitForSelector('#footer', { state: 'visible' });
+  const nav = await page.waitForSelector('#nav', { state: 'visible' });
 
-  // Get all links in footer
-  const links = await footer.$$eval('a', (allLinks) =>
+  // Get all links in navbar
+  const links = await nav.$$eval('a', (allLinks) =>
     allLinks.map((link) => ({
       href: link.href,
       text: link.innerText.trim()
@@ -15,8 +15,13 @@ test('Check all footer links are working', async ({ page }) => {
 
   // Iterate over each link and check if the URL returns a 200 status code
   for (const link of links) {
-    if (link.href.startsWith('mailto')) continue; //skip email links
     const response = await page.request.get(link.href);
     expect(response.status(), `Link "${link.text}" (${link.href}) is broken`).toBe(200);
   }
+
+  // CTA button
+  const button = page.getByRole('button', { name: /talk to mission expert/i });
+  await button.click();
+  await page.waitForLoadState('load');
+  await expect(page).toHaveURL(`/contact`);
 });
