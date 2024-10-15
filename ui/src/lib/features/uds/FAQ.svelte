ba5196164da106e406822def8873faebce887af0
Andrew Risse <andrewrisse@gmail.com>
2024-10-15 17:56:53 -0600

    restucture strings to fix lint

--- ui/src/lib/features/uds/FAQ.svelte
+++ ui/src/lib/features/uds/FAQ.svelte
@@ -6,122 +6,52 @@
     {
       question: 'What does it mean that applications are pre-integrated into UDS?',
       answer:
-        'Pre-integrated applications within UDS are those that have been fully configured, tested\n' +
-        '          and validated to work seamlessly on the UDS software delivery platform. These applications\n' +
-        "          are configured to take advantage of UDS's built-in capabilities, such as automated\n" +
-        '          security policies, monitoring, and container management, reducing the time and effort\n' +
-        '          required for deployment. The goal is to minimize the need for customization and ensure\n' +
-        '          that mission-critical applications can be deployed quickly and securely, to any\n' +
-        '          infrastructure.'
+        "Pre-integrated applications within UDS are those that have been fully configured, tested and validated to work seamlessly on the UDS software delivery platform. These applications are configured to take advantage of UDS's built-in capabilities, such as automated security policies, monitoring, and container management, reducing the time and effort required for deployment. The goal is to minimize the need for customization and ensure that mission-critical applications can be deployed quickly and securely, to any infrastructure."
     },
     {
       question: 'Do UDS applications require additional work to configure for deployment?',
       answer:
-        'UDS applications are pre-integrated, the platform may still require some configuration to\n' +
-        '          tailor them to specific mission environments. This is especially true for networking\n' +
-        '          settings, access policies, and any environment-specific customizations. However, the UDS\n' +
-        '          platform simplifies most of the deployment tasks by automating much of the configuration\n' +
-        '          process and streamlining operations.'
+        'UDS applications are pre-integrated, the platform may still require some configuration to tailor them to specific mission environments. This is especially true for networking settings, access policies, and any environment-specific customizations. However, the UDS platform simplifies most of the deployment tasks by automating much of the configuration process and streamlining operations.'
     },
     {
       question: 'Can UDS be deployed into any mission environment?',
       answer:
-        'Yes! UDS is highly flexible and can be deployed across a wide range of mission\n' +
-        '          environments, from major Cloud Service Providers (CSPs) to edge and on-premise\n' +
-        '          environments. The platform is specifically built for the DoD, supporting deployment in\n' +
-        '          environments that require both high security and operational agility. Already have a\n' +
-        '          specific cloud environment? We can deploy to AWS Gov Cloud (US) and Azure Government\n' +
-        '          Cloud. Need to deploy at a higher classification level or to an edge device? We can deploy\n' +
-        '          there too!'
+        'Yes! UDS is highly flexible and can be deployed across a wide range of mission environments, from major Cloud Service Providers (CSPs) to edge and on-premise environments. The platform is specifically built for the DoD, supporting deployment in environments that require both high security and operational agility. Already have a specific cloud environment? We can deploy to AWS Gov Cloud (US) and Azure Government Cloud. Need to deploy at a higher classification level or to an edge device? We can deploy there too!'
     },
     {
       question: 'Does UDS follow the DoD DevSecOps Reference Design?',
       answer:
-        ' Yes, UDS adheres to the DoD DevSecOps Reference Design. It follows the DevSecOps\n' +
-        '          principles of integrating security into the entire software lifecycle, automating testing,\n' +
-        '          and ensuring continuous monitoring. UDS incorporates security into development,\n' +
-        '          deployment, and operational processes to align with the DoD’s cybersecurity and software\n' +
-        '          modernization initiatives.'
+        ' Yes, UDS adheres to the DoD DevSecOps Reference Design. It follows the DevSecOps principles of integrating security into the entire software lifecycle, automating testing, and ensuring continuous monitoring. UDS incorporates security into development, deployment, and operational processes to align with the DoD’s cybersecurity and software modernization initiatives.'
     },
     {
       question: 'How does deploying applications with UDS accelerate the ATO process?',
       answer:
-        'UDS accelerates the Authorization to Operate (ATO) process by utilizing automated security\n' +
-        '          checks, pre-configured compliance with NIST SP 800-53 controls, and a continuous\n' +
-        '          monitoring approach. This reduces the manual documentation and validation steps typically\n' +
-        '          involved in ATO, significantly cutting down the time to deployment while ensuring security\n' +
-        '          compliance. We accelerate not only your initial ATO but also ensure continuous deployment\n' +
-        '          by automating security validations as part of the delivery process.'
+        'UDS accelerates the Authorization to Operate (ATO) process by utilizing automated security checks, pre-configured compliance with NIST SP 800-53 controls, and a continuous monitoring approach. This reduces the manual documentation and validation steps typically involved in ATO, significantly cutting down the time to deployment while ensuring security compliance. We accelerate not only your initial ATO but also ensure continuous deployment by automating security validations as part of the delivery process.'
     },
     {
       question: 'How would deploying an application with UDS impact security posture?',
       answer:
-        'Deploying an application with UDS enhances security posture by implementing security\n' +
-        '          controls within every runtime environment. The platform continuously monitors\n' +
-        '          applications, automatically enforces security policies, and integrates tools for\n' +
-        '          vulnerability scanning, ensuring that security risks are mitigated early and throughout\n' +
-        '          the application’s lifecycle.'
+        'Deploying an application with UDS enhances security posture by implementing security controls within every runtime environment. The platform continuously monitors applications, automatically enforces security policies, and integrates tools for vulnerability scanning, ensuring that security risks are mitigated early and throughout the application’s lifecycle.'
     },
     {
       question: 'Is SLA support for UDS available?',
       answer:
-        '          Yes, Defense Unicorns provides paid support to DoD missions with an SLA (Service Level\n' +
-        '          Agreement) that meets mission requirements, ensuring that users can receive the necessary\n' +
-        '          technical and operational support to maintain their mission capabilities running on UDS.\n' +
-        '          Our services provide continuous monitoring, technical support, vulnerability management,\n' +
-        '          and education, including:\n' +
-        '        </p>\n' +
-        '        <ul class="ml-8 list-disc">\n' +
-        '          <li class="text-gray-500 dark:text-gray-400">\n' +
-        '            <strong>24/7 support for critical issues</strong> to ensure rapid resolution of any high-priority\n' +
-        '            issues.\n' +
-        '          </li>\n' +
-        '          <li class="text-gray-500 dark:text-gray-400">\n' +
-        '            <strong>Live training sessions</strong> to assist users in effectively operating UDS, helping\n' +
-        '            to reduce downtime and operational risks.\n' +
-        '          </li>\n' +
-        '          <li class="text-gray-500 dark:text-gray-400">\n' +
-        '            <strong>Dedicated account management</strong> to provide personalized assistance and ensure\n' +
-        '            the deployment aligns with mission requirements.\n' +
-        '          </li>\n' +
-        '          <li class="text-gray-500 dark:text-gray-400">\n' +
-        '            <strong>CVE scans, industry leading secure images and risk mitigation</strong> which proactively\n' +
-        '            address security vulnerabilities.\n' +
-        '          </li>\n' +
-        '          <li class="text-gray-500 dark:text-gray-400">\n' +
-        '            <strong>Online or onsite installation and deployment support</strong> to guarantee seamless\n' +
-        '            integration and setup in any environment.\n' +
-        '          </li>\n' +
-        '        </ul>\n' +
-        '        <a href="/contact" style="color: #76a9fa;">Speak to one of our Mission Experts</a> about how to access an SLA which suits\n' +
-        '        your mission parameters.'
+        'Yes, Defense Unicorns provides paid support to DoD missions with an SLA (Service Level Agreement) that meets mission requirements, ensuring that users can receive the necessary technical and operational support to maintain their mission capabilities running on UDS. Our services provide continuous monitoring, technical support, vulnerability management, and education, including: </p> <ul class="ml-8 list-disc"> <li class="text-gray-500 dark:text-gray-400"> <strong>24/7 support for critical issues</strong> to ensure rapid resolution of any high-priority issues. </li> <li class="text-gray-500 dark:text-gray-400"> <strong>Live training sessions</strong> to assist users in effectively operating UDS, helping to reduce downtime and operational risks. </li> <li class="text-gray-500 dark:text-gray-400"> <strong>Dedicated account management</strong> to provide personalized assistance and ensure the deployment aligns with mission requirements. </li> <li class="text-gray-500 dark:text-gray-400"> <strong>CVE scans, industry leading secure images and risk mitigation</strong> which proactively address security vulnerabilities. </li> <li class="text-gray-500 dark:text-gray-400"> <strong>Online or onsite installation and deployment support</strong> to guarantee seamless integration and setup in any environment. </li> </ul> <a href="/contact" style="color: #76a9fa;">Speak to one of our Mission Experts</a> about how to access an SLA which suits your mission parameters.'
     },
     {
       question: 'Do the applications & services that are included with UDS cost extra?',
       answer:
-        'The UDS platform itself includes many core services and capabilities out of the box.\n' +
-        '          However, infrastructure as code, last mile configuration, training and secure images\n' +
-        '          involve additional support. Additionally, third-party software vendors may require\n' +
-        '          licenses depending on the feature set your mission needs. <a href="/contact" style="color: #76a9fa;">Our mission experts are available to support with pricing and contracting</a>.'
+        'The UDS platform itself includes many core services and capabilities out of the box. However, infrastructure as code, last mile configuration, training and secure images involve additional support. Additionally, third-party software vendors may require licenses depending on the feature set your mission needs. <a href="/contact" style="color: #76a9fa;">Our mission experts are available to support with pricing and contracting</a>.'
     },
     {
       question: 'Can I deploy an app on UDS that is not currently in the app store?',
       answer:
-        ' Yes, you can deploy an application that is not in the UDS app store. UDS supports custom\n' +
-        '          deployments, allowing users to integrate their own applications into the platform,\n' +
-        '          provided they follow the UDS packaging guidelines and are compatible with the underlying\n' +
-        '          Kubernetes environment. If you need help or want to request an application please reach\n' +
-        '          out <a href="/contact" style="color: #76a9fa;">here</a>.'
+        ' Yes, you can deploy an application that is not in the UDS app store. UDS supports custom deployments, allowing users to integrate their own applications into the platform, provided they follow the UDS packaging guidelines and are compatible with the underlying Kubernetes environment. If you need help or want to request an application please reach out <a href="/contact" style="color: #76a9fa;">here</a>.'
     },
     {
       question: 'Is UDS an extension of Platform One’s Big Bang?',
       answer:
-        ' Yes, UDS is an extension of Big Bang. UDS builds upon the security posture established by\n' +
-        '          Platform One’s Big Bang, by adding enhanced automation features through the UDS Operator\n' +
-        '          and UDS Policy Engine. While Big Bang focuses on deploying secure, compliant\n' +
-        '          infrastructure, UDS strengthens and extends Big Bang’s functionality, providing more\n' +
-        '          automation and security features specifically tailored for application deployment and\n' +
-        '          policy management. UDS can be found in <a target="_blank" href="https://repo1.dso.mil/platform-one/distros/defense-unicorns/uds-core" style="color: #76a9fa;">Platform One’s Repo1.</a>'
+        ' Yes, UDS is an extension of Big Bang. UDS builds upon the security posture established by Platform One’s Big Bang, by adding enhanced automation features through the UDS Operator and UDS Policy Engine. While Big Bang focuses on deploying secure, compliant infrastructure, UDS strengthens and extends Big Bang’s functionality, providing more automation and security features specifically tailored for application deployment and policy management. UDS can be found in <a target="_blank" href="https://repo1.dso.mil/platform-one/distros/defense-unicorns/uds-core" style="color: #76a9fa;">Platform One’s Repo1.</a>'
     }
   ];
 </script>
